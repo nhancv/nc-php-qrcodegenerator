bd7c7c937223b3fc4dad021810364c8c3e28fcd7
Nhan Cao <nhancv92@gmail.com>
2017-11-22 19:34:28 +0700

    update source

--- index.php
+++ index.php
@@ -1,7 +1,39 @@
 <?php 
 
-    include('phpqrcode/qrlib.php'); 
-     
-    // outputs image directly into browser, as PNG stream 
-    QRcode::png('PHP QR Code :)');
-    //http://phpqrcode.sourceforge.net/examples/index.php
+    $data = "http://nhancv.github.io/";
+    $author = "Nhan Cao";
+    $description = "QRCode generate";
+
+	if (isset($_GET['data'])) {
+	    $data = $_GET['data'];
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <!--SEO-->
+    <title>QRCode</title>
+    <meta http-equiv='Content-Type' content="text/html; charset=utf-8"/>
+
+    <!--Meta tag for facebook share-->
+    <meta property="og:url" content="<?php echo $data ?>"/>
+    <meta property="og:description" content="<?php echo $description ?>"/>
+    <meta property="og:image" content="qrcodegen.php?data='.$data.'"/>
+
+    <meta name="url" content="<?php echo $data ?>"/>
+    <meta name="author" content="<?php echo $author ?>" />
+    <meta name="description" content="<?php echo $description ?>" />
+    <meta name="image" content="qrcodegen.php?data='.$data.'"/>
+</head>
+<body>
+<img src="qrcodegen.php?data=$data">
+<br>
+<?php echo $data ?>
+
+</body>
+</html>
