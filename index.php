1c92b2eb0afafa8d25384c61de57c32606d6779c
Nhan Cao <nhancv92@gmail.com>
2017-11-22 21:50:31 +0700

    update

--- index.php
+++ index.php
@@ -19,26 +19,33 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en-US" prefix="og: http://ogp.me/ns#" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" itemscope itemtype="http://schema.org/Article">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name=viewport content="width=device-width, initial-scale=1">
     <!--SEO-->
     <title>QRCode</title>
-    <meta http-equiv='Content-Type' content="text/html; charset=utf-8"/>
+
+    <meta name="abstract" content="<?php echo $description ?>" >
+    <meta name="keywords" content="tools, qrcode, generator" >
+    <meta name="revisit-after" content="15" >
+    <meta name="language" content="EN" >
+    <meta name="copyright" content="© 2017 Nhan Cao" >
+    <meta name="robots" content="All" >
 
     <!--Meta tag for facebook share-->
-    <meta property="og:url" content="<?php echo $data ?>"/>
-    <meta property="og:description" content="<?php echo $description ?>"/>
-    <meta property="og:image" content="data:image/png;base64,<?php echo $image ?>"/>
+    <meta property="og:url" content="<?php echo $data ?>">
+    <meta property="og:description" content="<?php echo $description ?>">
+    <meta property="og:image" content="data:image/png;base64,<?php echo $image; ?>">
 
-    <meta name="url" content="<?php echo $data ?>"/>
-    <meta name="author" content="<?php echo $author ?>" />
-    <meta name="description" content="<?php echo $description ?>" />
-    <meta name="image" content="data:image/png;base64,<?php echo $image ?>"/>
+    <meta name="url" content="<?php echo $data ?>">
+    <meta name="author" content="<?php echo $author ?>" >
+    <meta name="description" content="<?php echo $description ?>" >
+    <meta name="image" content="data:image/png;base64,<?php echo $image; ?>">
+    <meta name="twitter:image" content="data:image/png;base64,<?php echo $image; ?>">
 </head>
 <body style="text-align: center;">
-<img src="data:image/png;base64,<?php echo $image ?>">
+<img src="data:image/png;base64,<?php echo $image; ?>">
 
 <div><?php echo $data ?></div>
 
